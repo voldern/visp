5666b2b257fa574f704b97b038ba5a700022bb01
Espen Volden <voldern@hoeggen.net>
2014-07-04 22:17:29 +0200

    Implement quote special form

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,5 +1,17 @@
+var ast = require('./ast');
+
+var specialForms = {
+    quote: function(args) {
+        return args[0];
+    }
+};
+
 function evaluate(sexp) {
-    return sexp;
+    if (!ast.is_list(sexp)) {
+        return sexp;
+    } else {
+        return specialForms[sexp[0]].call(sexp, sexp.slice(1));
+    }
 }
 
 module.exports = evaluate;
--- test/test_2_evaluating_simple_expressions.js
+++ test/test_2_evaluating_simple_expressions.js
@@ -17,3 +17,13 @@ test('evaluating integer', function(t) {
 
     t.equal(evaluate(42), 42);
 });
+
+test('evaluating quote', function(t) {
+    // When a call is done to the `quote` form, the argument should be returned
+    // without being evaluated.
+    // (quote foo) -> foo
+    t.plan(2);
+
+    t.equals(evaluate(['quote', 'foo']), 'foo');
+    t.looseEquals(evaluate(['quote', [1, 2, false]]), [1, 2, false]);
+});
